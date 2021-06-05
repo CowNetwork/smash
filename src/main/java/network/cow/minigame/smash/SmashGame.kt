eda909068c1c1c0e4bdfa67e5776a4413f6a53d1
Yannic Rieger <riegeryannic@gmail.com>
2021-06-05 17:42:35 +0200

    add license header and .gitattributes

--- src/main/java/network/cow/minigame/smash/SmashGame.kt
+++ src/main/java/network/cow/minigame/smash/SmashGame.kt
@@ -1,3 +1,20 @@
+/*
+    Smash mini game spigot plugin
+    Copyright (C) 2021  Yannic Rieger
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package network.cow.minigame.smash
 
 import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent
@@ -63,7 +80,6 @@ class SmashGame(game: Game<Player>, config: PhaseConfig<Player>) : SpigotPhase(g
     // TODO: track stats (kills etc.)
     // TODO: use itemBuilder
     // TODO: winning phase
-    // TODO: assign item to spawner until picked up so items wont overlap
 
     override fun onStart() {
         val worldMeta: WorldMeta = this.game.store.get("map") ?: error("no WorldMeta found")
